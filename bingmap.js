f2ffbe27ffbfc87d6eb65268f5bd6216ed73a7d2
dhoko <dhoko@.(none)>
2011-08-24 00:10:01 +0200

    v0.1.2 Fix the Pin (ZoomIn and ZoomOut)

--- bingmap.js
+++ bingmap.js
@@ -17,8 +17,6 @@
                 onFinish: function(obj) {},
                 api : 'Your Api Key', 
                 type : Microsoft.Maps.MapTypeId.road, // Raod Map
-                center_lat : 48.853225888012105, // French Reference
-                center_lng : 2.348883116245406, // French Reference
                 zoom : 16, 
                 height: 400,
                 width : 500,
@@ -38,25 +36,22 @@
  
             //Bing Map Creation : God says to Microsoft to do it
             map = new Microsoft.Maps.Map(this, {
-                credentials: options.api,
-                mapTypeId : options.type,
-                center: new Microsoft.Maps.Location(options.center_lat, options.center_lng),
-                zoom : options.zoom,
-                width : options.width,
-                height : options.height,
-                showScalebar : options.scaleBar,
-                showDashboard : options.control,
-                showLogo : options.bing_logo,
-                showCopyright : options.copy,
-                enableSearchLogo : options.search_logo,
-                disableKeyboardInput: true
-                });
+    			credentials: options.api,
+        		mapTypeId : options.type,
+    			center:  new Microsoft.Maps.Location(o.pin.lat, o.pin.lng),
+    			zoom : options.zoom,
+    			width : options.width,
+    			height : options.height,
+    			showScalebar : options.scaleBar,
+    			showDashboard : options.control,
+    			showLogo : options.bing_logo,
+    			showCopyright : options.copy,
+    			enableSearchLogo : options.search_logo,
+                enableClickableLogo: false,
+                disableKeyboardInput: true,
+    			});
 
         if(o.up) o.up.apply( this );
-      
-
-        // Retrieve the location of the map center 
-        var center = map.getCenter();
 
         // Add a pin to the map
             var pin = new Microsoft.Maps.Pushpin(
@@ -64,11 +59,7 @@
                         "latitude" : o.pin.lat, 
                         "longitude": o.pin.lng
                     },
-                    {
-                        icon: o.pin.icon,
-                        height:50,
-                        width:50,
-                        anchor:new Microsoft.Maps.Point(0,50),
+                    {                      
                         draggable: options.pin.draggable
                     }); 
 
