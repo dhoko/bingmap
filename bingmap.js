c0e99394d974424865067d468bb278e6dda0ae08
dhoko <dhoko@cyaneus.org>
2012-02-22 00:03:32 +0100

    doc in progress

--- bingmap.js
+++ bingmap.js
@@ -1,11 +1,8 @@
 ﻿/**
-* Bing Map v1 Display a map for jQuery 1.6
-* Release date : 24/08/2011
-* v 0.1.3
-* Changelog : Bug with the zoom if you specify a string
-
-* @author    aurelien[dot]garroux[at]gmail[dot]com
-* @author AurÃ©lien Garroux for Procheo
+* BingMap - Bing Map for jQuery
+* Release date : 21/02/2011
+* v 0.1.5
+* @author Aurélien Garroux for Procheo
 */
 
 (function($){
@@ -19,30 +16,40 @@
         latitude : 48.833,
         longitude : 2.333,
         pin : [],
-        options : {
-            bing_logo : false,
-            search_logo : false,
-            scaleBar : false,
-            control : false,
-            copy : false,
-        },
-        onFinish : function(obj){}
+        bing_logo : false,
+        search_logo : false,
+        scaleBar : false,
+        control : false,
+        copy : false,
+        can_zoom : false,
+        can_navigate : false,
+        onFinish : function(obj){},
+        init : function(options){
+            
+        }
     }
 
-    var typeMap = [{name : "road",def : Microsoft.Maps.MapTypeId.road},{name : "sky",def : Microsoft.Maps.MapTypeId.aerial}]
+    var typeMap = [{name: "road",def: Microsoft.Maps.MapTypeId.road},{name: "sky",def: Microsoft.Maps.MapTypeId.aerial},{name: "bird",def: Microsoft.Maps.MapTypeId.birdseye}]
     var type_show = Microsoft.Maps.MapTypeId.road;
     var pins = [];
+    var actZoom = true;
+    var actPan = true;
 
-    $.fn.bingmap = function(o){
-        if(o) var o = $.extend(config,o); 
+    $.fn.bingmap = function(op){
+        if(op) var o = $.extend(config,op); 
         
-        for(var i=0; i<=1; i++){
+        
+       
+
+        if(o.can_zoom == true) actZoom = false;
+        if(o.can_navigate == true) actPan = false;
+
+        for(var i=0; i<=2; i++){
             if(o.type == typeMap[i].name)
                 type_show = typeMap[i].def;
         } 
-
-        return this.each(function(){
-
+        return this.each(function(){           
+              
             var map = new Microsoft.Maps.Map(this, {
                 credentials: o.api,
                 mapTypeId : type_show,
@@ -50,17 +57,20 @@
                 zoom : parseFloat(o.zoom),
                 width : o.width,
                 height : o.height,
-                showScalebar : o.options.scaleBar,
+                showScalebar : o.scaleBar,
                 showMapTypeSelector: false,
-                showDashboard : o.options.control,
-                showLogo : o.options.bing_logo,
-                showCopyright : o.options.copy,
-                enableSearchLogo : o.options.search_logo,
+                showDashboard : o.control,
+                showLogo : o.bing_logo,
+                showCopyright : o.copy,
+                enableSearchLogo : o.search_logo,
                 enableClickableLogo: false,
                 disableKeyboardInput: true,
+                fixedMapPosition: true,
+                disableZooming: actZoom,
+                disablePanning: actPan
                 });
 
-
+        console.log(o);
         if(o.up) o.up.apply( this );
 
         if(o.pin.length > 0){
@@ -76,8 +86,9 @@
             });
         }
             //Callback Function
-        if(o.onFinish) o.onFinish.call(this, {pin : pins, map: map});     
-        
+        if(o.onFinish) o.onFinish.call(this, {pin: pins, map: map});     
+        map = {};
+        o = {};
         });
     }
     
