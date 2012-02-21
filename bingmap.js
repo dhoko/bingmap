f7a464f99895961732ee0c851eaf0e0cb5582c92
dhoko <dhoko@cyaneus.org>
2012-02-21 22:39:44 +0100

    Plus fonctionnel, multiples instances, multi pins....

--- bingmap.js
+++ bingmap.js
@@ -1,81 +1,84 @@
 ﻿/**
 * Bing Map v1 Display a map for jQuery 1.6
-* Release date : 23/08/2011
-* 
-* 
-* @param  f  onMouseOver function || An object with configuration options
-* @param  g  onMouseOut function  || Nothing (use configuration options object)
+* Release date : 24/08/2011
+* v 0.1.3
+* Changelog : Bug with the zoom if you specify a string
+
 * @author    aurelien[dot]garroux[at]gmail[dot]com
-* @author Aurelien Garroux for Procheo
+* @author AurÃ©lien Garroux for Procheo
 */
 
 (function($){
-    $.fn.mapbing = function(o){
+
+    var config = {
+        api : "Your API Key",
+        type : "road",
+        zoom : 5,
+        width : 500,
+        height : 400,
+        latitude : 48.833,
+        longitude : 2.333,
+        pin : [],
+        options : {
+            bing_logo : false,
+            search_logo : false,
+            scaleBar : false,
+            control : false,
+            copy : false,
+        },
+        onFinish : function(obj){}
+    }
+
+    var typeMap = [{name : "road",def : Microsoft.Maps.MapTypeId.road},{name : "sky",def : Microsoft.Maps.MapTypeId.aerial}]
+    var type_show = Microsoft.Maps.MapTypeId.road;
+    var pins = [];
+
+    $.fn.bingmap = function(o){
+        if(o) var o = $.extend(config,o); 
         
-          //Default Options  
-            var options = {
-                onFinish: function(obj) {},
-                api : 'Your Api Key', 
-                type : Microsoft.Maps.MapTypeId.road, // Raod Map
-                zoom : 16, 
-                height: 400,
-                width : 500,
-                bing_logo: true, // Display the Bing logo
-                search_logo : false, // Display the Bing search (hover(bing_logo)
-                scaleBar: false,
-                control: false,
-                copy : false,
-                pin : {
-                    draggable : false
-                    }
-                }
-        
-        if(o) $.extend(options,o);    
-        
+        for(var i=0; i<=1; i++){
+            if(o.type == typeMap[i].name)
+                type_show = typeMap[i].def;
+        } 
+
         return this.each(function(){
- 
-            //Bing Map Creation : God says to Microsoft to do it
-            map = new Microsoft.Maps.Map(this, {
-    			credentials: options.api,
-        		mapTypeId : options.type,
-    			center:  new Microsoft.Maps.Location(o.pin.lat, o.pin.lng),
-    			zoom : options.zoom,
-    			width : options.width,
-    			height : options.height,
-    			showScalebar : options.scaleBar,
-    			showDashboard : options.control,
-    			showLogo : options.bing_logo,
-    			showCopyright : options.copy,
-    			enableSearchLogo : options.search_logo,
+
+            var map = new Microsoft.Maps.Map(this, {
+                credentials: o.api,
+                mapTypeId : type_show,
+                center:  new Microsoft.Maps.Location(o.latitude, o.longitude),
+                zoom : parseFloat(o.zoom),
+                width : o.width,
+                height : o.height,
+                showScalebar : o.options.scaleBar,
+                showMapTypeSelector: false,
+                showDashboard : o.options.control,
+                showLogo : o.options.bing_logo,
+                showCopyright : o.options.copy,
+                enableSearchLogo : o.options.search_logo,
                 enableClickableLogo: false,
                 disableKeyboardInput: true,
-    			});
+                });
+
 
         if(o.up) o.up.apply( this );
 
-        // Add a pin to the map
-            var pin = new Microsoft.Maps.Pushpin(
-                    {
-                        "latitude" : o.pin.lat, 
-                        "longitude": o.pin.lng
-                    },
-                    {                      
-                        draggable: options.pin.draggable
-                    }); 
-
-        // Add a pin on your map
-        map.entities.push(pin);
-        
-        //Object to allow interaction with map &pin    
-        var obj = {pin: pin, map: map};
-
-        //Set a custom ClassName to the pin
-        pin.cm1001_er_etr.dom.className = "pinMap";
-
+        if(o.pin.length > 0){
+            $(o.pin).each(function(n,e){
+               if(!e.drag) e.drag = false;
+                var pin = new Microsoft.Maps.Pushpin({
+                            "latitude" : e.lat, 
+                            "longitude": e.lng},{                      
+                                draggable: e.drag
+                            });
+                map.entities.push(pin);
+                pins[n] = pin;
+            });
+        }
             //Callback Function
-            if(o.onFinish) o.onFinish.call(this, obj);     
+        if(o.onFinish) o.onFinish.call(this, {pin : pins, map: map});     
         
-         });
+        });
     }
     
     
